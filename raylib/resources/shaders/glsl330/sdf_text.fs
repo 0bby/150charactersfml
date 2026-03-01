0b4a27b5b59a4f90706e27109faa94de2deb3a1e
olifog <oliverfogelin@gmail.com>
2026-03-01 13:34:00 +0000

    initial font fix

---
+++ raylib/resources/shaders/glsl330/sdf_text.fs
@@ -0,0 +1,17 @@
+#version 330
+
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+uniform sampler2D texture0;
+uniform vec4 colDiffuse;
+
+out vec4 finalColor;
+
+void main()
+{
+    float d = texture(texture0, fragTexCoord).a;
+    float aaf = fwidth(d);
+    float alpha = smoothstep(0.5 - aaf, 0.5 + aaf, d);
+    finalColor = vec4(fragColor.rgb * colDiffuse.rgb, fragColor.a * colDiffuse.a * alpha);
+}
